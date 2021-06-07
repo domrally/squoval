3bea29df4d7619226b579b501c19c8bd4e6b4c1d
Dom <dominicemandy@gmail.com>
2021-06-07 00:04:35 -0700

    potential performance

--- src/houdini/houdini.js
+++ src/houdini/houdini.js
@@ -25,19 +25,16 @@ const clamp = (x, lowerlimit, upperlimit) => {
 // 
 const getAlpha = (x, y, unit) => {
     let alpha = 1
-    // check if outside of minimum radius
-    if (x * x + y * y > 1.5) {
-        // the bottom left quadrant appears to be the most numerically stable
-        x = -Math.abs(x)
-        y = -Math.abs(y)
-        // since the shape is convex we can be sure which points are inside
-        const dx = singd(acosgd(x)) - y + unit
-        if (dx > 0) {
-            const dy = cosgd(asingd(y)) - x + unit
-            // distance field becomes asymptotically correct as points approach curve
-            const d = dx * dy / Math.sqrt(dx * dx + dy * dy)
-            alpha = smoothstep(2 * unit, 0, d)
-        }
+    // the bottom left quadrant appears to be the most numerically stable
+    x = -Math.abs(x)
+    y = -Math.abs(y)
+    // since the shape is convex we can be sure which points are inside
+    const dx = singd(acosgd(x)) - y + unit
+    if (dx > 0) {
+        const dy = cosgd(asingd(y)) - x + unit
+        // distance field becomes asymptotically correct as points approach curve
+        const d = dx * dy / Math.sqrt(dx * dx + dy * dy)
+        alpha = smoothstep(2 * unit, 0, d)
     }
 
     return alpha
@@ -99,28 +96,33 @@ registerPaint('corner-shape', class {
 
         for (let i = 0; i < radius; i += .5) {
             for (let j = 0; j <= i; j += .5) {
-                const alpha = getAlpha(.5 * Math.PI * (1 - i / (radius - 1)), .5 * Math.PI * (1 - j / (radius - 1)), unit)
-                if (alpha) {
-                    ctx.fillStyle = `rgb(0, 0, 0, ${alpha})`
+                const z = 1 - i / (radius - 1)
+                const w = 1 - i / (radius - 1)
+                // check if outside of minimum radius
+                if (z * z + w * w > 4) {
+                    const alpha = getAlpha(.5 * Math.PI * z, .5 * Math.PI * w, unit)
+                    if (alpha) {
+                        ctx.fillStyle = `rgb(0, 0, 0, ${alpha})`
 
-                    const s = .75
-                    const u = .5
-                    ctx.fillRect(i, j, s, s)
-                    if (j !== i) ctx.fillRect(j, i, s, s)
+                        const s = .75
+                        const u = .5
+                        ctx.fillRect(i, j, s, s)
+                        if (j !== i) ctx.fillRect(j, i, s, s)
 
-                    const a = geom.width - i - u
-                    const b = geom.height - j - u
-                    const c = geom.width - j - u
-                    const d = geom.height - i - u
+                        const a = geom.width - i - u
+                        const b = geom.height - j - u
+                        const c = geom.width - j - u
+                        const d = geom.height - i - u
 
-                    ctx.fillRect(a, b, s, s)
-                    ctx.fillRect(c, d, s, s)
+                        ctx.fillRect(a, b, s, s)
+                        ctx.fillRect(c, d, s, s)
 
-                    ctx.fillRect(i, b, s, s)
-                    ctx.fillRect(c, i, s, s)
-                    
-                    ctx.fillRect(a, j, s, s)
-                    ctx.fillRect(j, d, s, s)
+                        ctx.fillRect(i, b, s, s)
+                        ctx.fillRect(c, i, s, s)
+                        
+                        ctx.fillRect(a, j, s, s)
+                        ctx.fillRect(j, d, s, s)
+                    }
                 }
             }
         }
