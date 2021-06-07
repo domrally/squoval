0b4afa60cadce5add860aba93c09822bc4924810
Dom <dominicemandy@gmail.com>
2021-06-06 19:43:48 -0700

    bigger offset

--- src/houdini/houdini.js
+++ src/houdini/houdini.js
@@ -32,8 +32,8 @@ const getAlpha = (x, y, size) => {
         x = -Math.abs(x)
         y = -Math.abs(y)
         // since the shape is convex we can be sure which points are inside
-        const dx = singd(acosgd(x)) - y - 1 / size
-        const dy = cosgd(asingd(y)) - x - 1 / size
+        const dx = singd(acosgd(x)) - y - 4 / size
+        const dy = cosgd(asingd(y)) - x - 4 / size
         if (dx < 0 || dy < 0) {
             // distance field becomes asymptotically correct as points approach curve
             const d = dx * dy / Math.sqrt(dx * dx + dy * dy)
