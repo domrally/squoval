a2aa20555c39a6e5590426fb2781100e299a847c
Dom <dominicemandy@gmail.com>
2021-06-06 19:01:09 -0700

    maybe they always match

--- src/houdini/houdini.js
+++ src/houdini/houdini.js
@@ -34,7 +34,7 @@ const getAlpha = (x, y, width, height) => {
         // since the shape is convex we can be sure which points are inside
         const dx = singd(acosgd(x)) - y
         const dy = cosgd(asingd(y)) - x
-        if (dx < 0 && dy < 0) {
+        if (dx < 0) {
             // distance field becomes asymptotically correct as points approach curve
             const d = dx * dy / Math.sqrt(dx * dx + dy * dy)
             alpha = clamp(d, 0, 1)
