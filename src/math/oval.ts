6d392dd0a990c099524e1fcc711f9f61220716fc
Dom <dominicemandy@gmail.com>
2022-01-19 23:09:50 -0800

    Update and rename gdcircle.js to oval.ts

--- src/math/oval.ts
+++ src/math/oval.ts
@@ -4,6 +4,12 @@ function erf(t: number) {
     (2 * Math.sqrt(t) + Math.sqrt(t + 9))
 }
 
+function oval(t: number) {
+  const x = erf(Math.cos(t) / Math.abs(Math.sin(t)))
+  const y = erf(Math.sin(t) / Math.abs(Math.cos(t)))
+  return { x, y }
+}
+
 // plot x = erf(cos t |csc t|), y = erf(sin t |sec t|)
 
 `((x), (y)) = "erf" ((cos t |csc t|), (sin t |sec t|)`
