9405acf47ea0cd67cbc5c02fc230dd4b45da9b8e
Dom <dominicemandy@gmail.com>
2022-01-19 23:41:01 -0800

    Revert "Revert "Update oval.ts""
    This reverts commit abf177db9d2ee10a3c333a06ab2d54bb97b58eaf.

--- src/math/oval.ts
+++ src/math/oval.ts
@@ -16,6 +16,6 @@ function oval(t: number) {
 
 // plot x = erf(cos t |csc t|), y = erf(sin t |sec t|)
 
-`((x), (y)) = "erf" ((cos t), (sin t):} |(csc t),(sec t)|)`
+`((x), (y)) = "erf" ((cos t / |sin t|), (sin t / | cos t |))`
 
 // the never ending search
