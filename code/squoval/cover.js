93a2287cd32c4b862fd454798f1e9ca6d7c4b45a
Dom <dominicemandy@gmail.com>
2022-01-21 15:03:12 -0800

    performance

--- code/squoval/cover.js
+++ code/squoval/cover.js
@@ -1,10 +1,10 @@
 export const cover = (width = 0, height = 0, curve = () => ({ x: 0, y: 0})) => t => {   
-    const { min, sign } = Math;
-    const centerX = width / 2;
-    const centerY = height / 2;
-    const centerMin = min(centerX, centerY);
-    const offsetX = centerX - centerMin;
-    const offsetY = centerY - centerMin;
+    const { min, sign } = Math,
+        centerX = width / 2,
+        centerY = height / 2,
+        centerMin = min(centerX, centerY),
+        offsetX = centerX - centerMin,
+        offsetY = centerY - centerMin;
 
     let { x, y } = curve(t);
 
