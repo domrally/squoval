dfb4b22cc8210ef6524bbfdfe84605e96a44fcb5
Dom <dominicemandy@gmail.com>
2022-01-21 16:19:55 -0800

    clip path

--- code/squoval-mask.js
+++ code/squoval-mask.js
@@ -4,51 +4,25 @@ class SquovalMask extends HTMLElement {
     constructor() {
         super();
 
-        const shadow = this.attachShadow({ mode: 'closed' });
-        shadow.innerHTML = `<canvas style="display: none;"></canvas><style>:host {display: block;}</style>`;
-
-        const { firstElementChild } = shadow,
-            context = firstElementChild.getContext('2d');
-        
-        context.fillStyle = '#ffff';
-
-        const onResize = new ResizeObserver(this.resize.bind(this, firstElementChild, context));
+        const onResize = new ResizeObserver(this.resize.bind(this));
         onResize.observe(this);
     }
     
-    resize(canvas, context) {
+    resize() {
         const { PI } = Math,
             { clientWidth, clientHeight, style } = this;
-
-        canvas.width = clientWidth;
-        canvas.height = clientHeight;
-        
-        context.beginPath();
-        context.moveTo(clientWidth, clientHeight / 2);
     
         const curve = squoval(clientWidth, clientHeight),
             step = 64 * PI / (clientWidth + clientHeight);
 
-        let X = 0, Y = 0;
-
+        let polygon = ''
         for (let t = step / 2; t < 2 * PI; t += step) {
             const { x, y } = curve(t);
-            
-            const a0 = 2 * X / 3 + x / 3,
-                b0 = 2 * Y / 3 + y / 3,
-                a1 = X / 3 + 2 * x / 3,
-                b1 = Y / 3 + 2 * y / 3;
 
-            context.bezierCurveTo(a0, b0, a1, b1, x, y);
+            polygon += `${ x.toFixed(3) }% ${ y.toFixed(3) }%, `;
         }
-    
-        context.closePath();
-        context.fill();
-    
-        const maskImage = `url(${ canvas.toDataURL() })`;
-    
-        style.maskImage = maskImage;
-        style.webkitMaskImage = maskImage;
+
+        style.clipPath = `polygon(${ polygon })`;
     }
 }
 
--- code/squoval/cover.js
+++ code/squoval/cover.js
@@ -1,15 +1,8 @@
 export const cover = (width = 0, height = 0, curve = () => ({ x: 0, y: 0})) => t => {   
-    const { min, sign } = Math,
-        centerX = width / 2,
-        centerY = height / 2,
-        centerMin = min(centerX, centerY),
-        offsetX = centerX - centerMin,
-        offsetY = centerY - centerMin;
-
     let { x, y } = curve(t);
 
-    x = centerX + offsetX * sign(x) + centerMin * x;
-    y = centerY + offsetY * sign(y) + centerMin * y;
+    x++, y++;
+    x *= 50, y *= 50;
 
     return { x, y };
 }
