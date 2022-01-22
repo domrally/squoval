a7150ad7a4659838115ce4dd65ffbe928ba01ff5
Dom <dominicemandy@gmail.com>
2022-01-21 17:36:38 -0800

    Update squoval-mask.js

--- code/squoval-mask.js
+++ code/squoval-mask.js
@@ -16,11 +16,11 @@ class SquovalMask extends HTMLElement {
             step = 2 * PI / (clientWidth + clientHeight);
 
         const initial = curve(step / 2)
-        let polygon = `${ initial.x.toFixed(2) }% ${ initial.y.toFixed(2) }%`;
+        let polygon = `${ initial.x.toFixed(1) }% ${ initial.y.toFixed(1) }%`;
         for (let t = -3 * step / 2; t < 2 * PI; t -= step) {
             const { x, y } = curve(t);
 
-            polygon += `, ${ x.toFixed(2) }% ${ y.toFixed(2) }%`;
+            polygon += `, ${ x.toFixed(1) }% ${ y.toFixed(1) }%`;
         }
 
         style.clipPath = `polygon(${ polygon })`;
