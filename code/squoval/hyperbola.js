ad5817056bbc9378ec3822e963310b97ed3f4f97
Dom <dominicemandy@gmail.com>
2022-01-22 11:28:17 -0800

    fix closure

--- code/squoval/hyperbola.js
+++ code/squoval/hyperbola.js
@@ -1,8 +1,8 @@
-export const hyperbola = () => t => {
+export const hyperbola = (t = 0) => {
     const { abs, cos, sin } = Math,
 
-        x = t => abs(1 / cos(t)), 
-        y = t => abs(1 / sin(t))
+        x = abs(1 / cos(t)), 
+        y = abs(1 / sin(t))
 
     return { x, y }
 }
--- code/squoval/square.js
+++ code/squoval/square.js
@@ -1,12 +1,12 @@
 import { circle } from "./circle.js"
 
-export const square = () => (t = 0) => {
+export const square = (t = 0) => {
     const { min } = Math,
 
         { x: sine, y: cosine } = circle(t),
         { x: cosecant, y: secant } = hyperbola(t)
         minimum = min(cosecant, secant),
-        
+
         x = minimum * cosine,
         y = minimum * sine
 
