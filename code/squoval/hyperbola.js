c7aa322fa806a62a821e6176e2bc1cf3228910c6
Dom <dominicemandy@gmail.com>
2022-01-22 11:43:17 -0800

    Update hyperbola.js

--- code/squoval/hyperbola.js
+++ code/squoval/hyperbola.js
@@ -1,8 +1,8 @@
 export const hyperbola = (t = 0) => {
     const { abs, cos, sin } = Math,
 
-        x = abs(1 / cos(t)), 
-        y = abs(1 / sin(t))
+        x = abs(1 / sin(t)), 
+        y = abs(1 / cos(t))
 
     return { x, y }
 }
