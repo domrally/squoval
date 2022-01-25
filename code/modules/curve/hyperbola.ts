ed839d5a5d45ca5413b402b4a1022186c4836483
Dom <dominicemandy@gmail.com>
2022-01-25 15:04:02 -0800

    fmt

--- code/modules/curve/hyperbola.ts
+++ code/modules/curve/hyperbola.ts
@@ -1,8 +1,8 @@
 export const hyperbola = (t = 0) => {
-    //
-    // ╭ x ╮   ╷ csct ╷
-    // │   │ = │      │
-    // ╰ y ╯   ╵ sect ╵
+  //
+  // ╭ x ╮   ╷ csct ╷
+  // │   │ = │      │
+  // ╰ y ╯   ╵ sect ╵
   const {abs, cos, sin} = Math,
     x = abs(1 / sin(t)),
     y = abs(1 / cos(t));
