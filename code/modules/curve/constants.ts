dc9cda4d34ad4ec6d4fdef1999dfab3acf2bf43a
Dom <dominicemandy@gmail.com>
2022-01-25 14:30:47 -0800

    Update constants.ts

--- code/modules/curve/constants.ts
+++ code/modules/curve/constants.ts
@@ -1,9 +1,13 @@
 // https://en.wikipedia.org/wiki/Error_function#Approximation_with_elementary_functions
-// alternative
-// export const a = .147;
 const {PI: π} = Math;
 //
+// accurate in a neighborhood of 0 and ∞
+// relative error less than 0.00035
 //   8   (π - 3)
 //  ───────────── ≈ 0.140012
 //   3π  (4 - π)
 export const a = (8 * (π - 3)) / (3 * π * (4 - π));
+// 
+// alternate value reduces the maximum
+// relative error to about 0.00013
+// export const a = .147;
