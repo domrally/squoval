9d7f79232263d0e0b24499ce6b9781587ced0e51
Dom <dominicemandy@gmail.com>
2022-01-27 13:36:15 -0800

    for matching 22.5% border radius

--- code/exports/curve.ts
+++ code/exports/curve.ts
@@ -4,6 +4,7 @@ import {circle} from '../modules/curve/circle.js';
 import {hyperbola} from '../modules/curve/hyperbola.js';
 
 // https://domrally.github.io/squoval
+const k = 1.06563394423898334607794393531990560241311032270502186985589866460684381517;
 export const Curve =
   (aspectRatio = 1) =>
   (t = 0) => {
@@ -14,8 +15,8 @@ export const Curve =
     // ╰   ╯         ╰                  ╯
     const {x: cosine, y: sine} = circle(t),
       {x: cosecant, y: secant} = hyperbola(t),
-      x = errorFunction(cosine * cosecant),
-      y = errorFunction(sine * secant);
+      x = errorFunction(k * cosine * cosecant),
+      y = errorFunction(k * sine * secant);
 
     return cover(aspectRatio, x, y);
   };
